44138ba3604627a9c45dd29d74db73d7ac46d9f9
go-bineanshi <bineanshi@163.com>
2024-06-28 17:24:37 +0800

    ping return pong at time

--- controllers/board.go
+++ controllers/board.go
@@ -1,11 +1,15 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"time"
+)
 
 type DemoController struct{}
 
 func (dc *DemoController) PingHandler(ctx *gin.Context) {
 	ctx.JSON(200, gin.H{
-		"message": "Pong",
+		"message": fmt.Sprintf("Pong at %s", time.Now().String()),
 	})
 }
